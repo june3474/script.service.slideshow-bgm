ed16b6cbdc5e3b6bc67933336bb9e50adbff6856
dks <june3474@gmail.com>
2021-11-16 06:45:42 +0900

    implement service.py

--- resources/lib/binder.py
+++ resources/lib/binder.py
@@ -5,13 +5,13 @@ import xml.etree.ElementTree as ET
 import xbmc, xbmcvfs
 from . import addon, addonId
 
-class Binder():
-    """Helper class used to hook slideshow-BGM addon up to the current skin.
+class Binder(target=Binder.find_SlideShow_xml()):
+    """Helper class used to tie slideshow-BGM addon to the current skin.
 
     """
 
     def __init__(self):
-        self.target = self.find_SlideShow_xml()
+        self.target = target
         self.parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))  # preserve comments
         self.tree = ET.parse(self.target, self.parser)
         self.root = self.tree.getroot()
@@ -19,8 +19,7 @@ class Binder():
         self.element.attrib = {'condition': 'System.HasAddon(%s)' % addonId + ' + ' + 'System.AddonIsEnabled(%s)' % addonId}
         self.element.text = 'RunAddon(%s)' % addonId
         
-    @staticmethod
-    def indent(elem, level=0):
+    def indent(self, elem, level=0):
         """Arrange indentations of a xml tree.
 
         Copied from <http://effbot.org/zone/element-lib.htm#prettyprint>_
@@ -38,15 +37,15 @@ class Binder():
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
             for elem in elem:
-                Hooker.indent(elem, level + 1)
+                self.indent(elem, level + 1)
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
         else:
             if level and (not elem.tail or not elem.tail.strip()):
                 elem.tail = i
 
-
-    def find_SlideShow_xml(self):
+    @staticmethod
+    def find_SlideShow_xml():
         """Locate ``SlideShow.xml`` file of the current skin.
 
         Returns:
@@ -64,13 +63,12 @@ class Binder():
 
         return path
 
-
     def check_hooked(self):
-        """Check current skin's SlideShow.xml is hooked up to ``slideshow-BGM`` of the current
+        """Check current skin's SlideShow.xml is tied to ``slideshow-BGM`` of the current
         skin.
 
-        Technically, check ``<onload *with ``self.element.attrib``*>RunAddon(script.slideshow-BGM)</onload>``
-        node exists in the `SlideShow.xml` file of the current skin.
+        Technically, check ``<onload  condition="System.HasAddon(script.slideshow-BGM) + System.AddonIsEnabled(script.slideshow-BGM)">
+        RunAddon(script.slideshow-BGM)</onload>`` node exists in the `SlideShow.xml` file of the current skin.
 
         Returns:
             tag(:class:`xml.etree.ElementTree.Element`):  tag if it's hooked, None otherwise.
@@ -83,10 +81,9 @@ class Binder():
             else:
                 return None
 
-
     def insert_tag(self):
-        """Insert tag ``<onload *with ``self.element.attrib``*>RunAddon(script.slideshow-BGM)</onload>``
-        into `SlideShow.xml` file of the current skin.
+        """Insert tag ``<onload condition="System.HasAddon(script.slideshow-BGM) + System.AddonIsEnabled(script.slideshow-BGM)">
+        RunAddon(script.slideshow-BGM)</onload>`` into `SlideShow.xml` file of the current skin.
 
         Returns:
             bool: True if suceeds, False otherwise.
@@ -100,10 +97,9 @@ class Binder():
         else:
             return False
 
-
     def remove_tag(self):
-        """Remove tag ``<onload> RunAddon(script.slideshow-BGM) </onload>``
-        from `SlideShow.xml` file of the current skin.
+        """Remove tag ``<onload condition="System.HasAddon(script.slideshow-BGM) + System.AddonIsEnabled(script.slideshow-BGM)"> 
+        RunAddon(script.slideshow-BGM) </onload>`` from `SlideShow.xml` file of the current skin.
 
         Returns:
             bool: True if suceeds, False otherwise.
--- service.py
+++ service.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
  
 import xbmc
+from resources.lib import addonId
 from resources.lib.binder import Binder
+from resources.lib.utils import log, show_dialog
 
+binder = Binder()
+
+if not binder.check_hooked():
+    binder.insert_tag()
 
